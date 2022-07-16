84f95a7a50a18f92efc1cb34f747a2f57c9dc763
Pratham-01 <prathamjajodia1@gmail.com>
2022-07-17 01:32:09 +0530

    static root

--- BloodDonation/settings.py
+++ BloodDonation/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -18,6 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'mbhe2&a(b&k-9mb__%vb6k%2+7&5d+=%$c_m(d67kq(f64i75('
